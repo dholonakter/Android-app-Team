55a85d00da0deec61d773cb022b38be58eb08237
Al Saleh,Meyra M.A <m.alsaleh@student.fontys.nl>
2021-10-12 18:00:27 +0200

    Update people

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,7 +5,8 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -24,11 +25,9 @@ import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity implements TokenFragment.OnFragmentInteractionListener {
 
-    public static MainActivity instance;
-    private PersonAdapter adapter;
-    private PeopleFragment peopleFragment;
-    private ViewPager viewPager;
-    private TabLayout allTabs;
+    private FragmentAdapter adapter;
+    private ViewPager2 pager2;
+    private TabLayout tabLayout;
 
     private TextView tv;
     private String token;
@@ -37,51 +36,55 @@ public class MainActivity extends AppCompatActivity implements TokenFragment.OnF
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv = findViewById(R.id.tv1);
-
-//        Button btn1 = findViewById(R.id.btn1);
-//        btn1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                // start PeopleActivity for result
-//                Intent intent = new Intent(MainActivity.this, PeopleFragment.class);
-//                startActivityForResult(intent, 1);
-//            }
-//        });
-
-//        allTabs = findViewById(R.id.tabs);
+        tv = (TextView) findViewById(R.id.tv1);
     }
 
-//    public static MainActivity getInstance() {
-//        return instance;
-//    }
-//    private void getAllWidgets() {
-//        viewPager = (ViewPager) findViewById(R.id.viewpager);
-//        allTabs = (TabLayout) findViewById(R.id.tabs);
-//    }
-
-//    private void setupViewPager() {
-//        adapter = new PersonAdapter(getSupportFragmentManager());
-//        peopleFragment = new PeopleFragment();
-//        adapter.addFragment(peopleFragment, "People");
-//        setViewPageAdapter();
-//    }
-
-//    private void setViewPageAdapter() {
-//        viewPager.setAdapter(adapter);
-//        allTabs.setupWithViewPager(viewPager);
-//    }
     @Override
     public void onFragmentInteraction(String token) {
+
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        pager2 = (ViewPager2) findViewById(R.id.view_pager2);
+
+        FragmentManager fm = getSupportFragmentManager();
+        adapter = new FragmentAdapter(fm, getLifecycle());
+        pager2.setAdapter(adapter);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Schedule"));
+        tabLayout.addTab(tabLayout.newTab().setText("People"));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        pager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+
         this.token = token;
         new JSONTask().execute(token);
 
     }
-    public class JSONTask extends AsyncTask<String, Void, List<Person>> {
+    public class JSONTask extends AsyncTask<String, Void, String> {
 
-        protected List<Person> doInBackground(String... params){
+        protected String doInBackground(String... params){
             URL url = null;
-            String s = null;
+            String s = "";
             List<Person> result = new ArrayList<>();
             try {
                 url = new URL("https://api.fhict.nl/people");
@@ -106,7 +109,7 @@ public class MainActivity extends AppCompatActivity implements TokenFragment.OnF
                     String telephoneNumber = onePerson.getString("telephoneNumber");
 
                     // add new person to list
-                    result.add(new Person(givenName, surName, initials, mail, telephoneNumber));
+//                    result.add(new Person(givenName, surName, initials, mail, telephoneNumber));
                 }
 
             } catch (IOException e) {
@@ -114,13 +117,13 @@ public class MainActivity extends AppCompatActivity implements TokenFragment.OnF
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-            return result;
+            return s;
         }
 
         @Override
-        protected void onPostExecute(List<Person> people){
-            super.onPostExecute(people);
-            tv.setText(people.toString());
+        protected void onPostExecute(String dataFetched){
+            super.onPostExecute(dataFetched);
+            tv.setText(dataFetched);
         }
     }
 }
--- app/src/main/java/com/example/myapplication/PeopleFragment.java
+++ app/src/main/java/com/example/myapplication/PeopleFragment.java
@@ -1,9 +1,11 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
@@ -11,6 +13,21 @@ import androidx.fragment.app.Fragment;
 public class PeopleFragment extends Fragment {
 
     private View convertView;
+
+    public PeopleFragment() {
+        // Required empty public constructor
+    }
+
+    public static PeopleFragment newInstance(String param1, String param2) {
+        PeopleFragment fragment = new PeopleFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,23 +37,23 @@ public class PeopleFragment extends Fragment {
             // if it is new, initialise it using 'fragment_json' layout
             // Inflate the layout for this fragment
             convertView = inflater.inflate(R.layout.fragment_people, container, false);
+
         }
 
         ListView listView = convertView.findViewById(R.id.lview1);
 
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                Object o = listView.getItemAtPosition(position);
+                Person person = (Person) o;
+                String telephoneNumber = person.telephoneNumber;
+                Log.d("TELEPHONE NUMBER", telephoneNumber);
+            }
+        });
+
         return convertView;
     }
-//    private void getAllWidgets(View view) {
-//        listView = (ListView) view.findViewById(R.id.listPeople);
-//        allPeople = getResources().obtainTypedArray(R.array.people);
-//    }
-//    private void setAdapter()
-//    {
-//        for (int i = 0; i < allPeople.length(); i++) {
-//            allPeopleNames.add(allPeople.getString(i));
-//        }
-//        PeopleListViewAdapter listViewAdapter= new PeopleListViewAdapter(MainActivity.getInstance(), al);
-//        listView.setAdapter(listViewAdapter);
-//    }
 
 }
